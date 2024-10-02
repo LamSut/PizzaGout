833200a97b777df6e4d71df746bdc04f56d0919d
LamSut <lamsut0007@gmail.com>
2024-10-02 12:20:32 +0700

    images

--- backend-api/src/app.js
+++ backend-api/src/app.js
@@ -18,6 +18,8 @@ app.get('/', (req, res) => {
     return res.json(JSend.success());
 });
 
+app.use('/public', express.static('public'));
+
 productRouter.setup(app);
 //404 res
 app.use(resourceNotFound);
