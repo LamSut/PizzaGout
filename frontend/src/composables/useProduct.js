0973ae7e42d93050499bc5550deabeb5d1abdc5d
LamSut <lamsut0007@gmail.com>
2024-11-10 20:49:09 +0700

    nil hil

--- frontend/src/composables/useProduct.js
+++ frontend/src/composables/useProduct.js
@@ -1,12 +1,8 @@
 import { useQuery } from '@tanstack/vue-query';
 import productService from '@/services/product.service';
 import { computed } from 'vue';
-import { useRouter, useRoute } from 'vue-router';
 
 export default function useProduct() {
-  const router = useRouter();
-  const route = useRoute();
-
   function fetchProducts(page) {
     const { data: productsPage, ...rest } = useQuery({
       queryKey: ['products', page],
@@ -18,22 +14,9 @@ export default function useProduct() {
   }
 
   function fetchProduct(id) {
-    const { data: product, error } = useQuery({
+    const { data: product } = useQuery({
       queryKey: ['products', id],
-      queryFn: async () => {
-        try {
-          return await productService.fetchProduct(id);
-        } catch (error) {
-          console.error('Error fetching product:', error);
-          router.push({
-            name: 'notfound',
-            params: { pathMatch: route.path.split('/').slice(1) },
-            query: route.query,
-            hash: route.hash
-          });
-          return null;
-        }
-      }
+      queryFn: () => productService.fetchProduct(id)
     });
     return product;
   }
