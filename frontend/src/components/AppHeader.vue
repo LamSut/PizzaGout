4c7d999a45bd35c010cbc563a64851a03799f216
Truong Dang Truc Lam <116415510+LamSut@users.noreply.github.com>
2025-06-12 19:59:26 +0700

    Update AppHeader.vue

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand bg-dark" data-bs-theme="dark" style="padding-bottom: 0px;">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand fw-bold" style="width: 200px; padding: 0px;">
-        <img src="/pizza.png" style="width: 20%; margin: 0px 10px 0px 0px" />
+        <img src="/pizza.png" style="width: 50%; margin: 0px 10px 0px 0px" />
         <h3 style="display: inline;">PIZZAGOUT</h3>
       </router-link>
     </div>
