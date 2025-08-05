17439ac68ed0ce9ffe339bc7b6e713d8614ec1d6
Truong Dang Truc Lam <116415510+LamSut@users.noreply.github.com>
2025-08-05 10:08:37 +0700

    Update AppHeader.vue

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand fw-bold" style="width: 200px; padding: 0px;">
         <img src="/pizza.png" style="width: 20%; margin: 0px 10px 0px 0px" />
-        <h3 style="display: inline;">PIZZAGOUT UPDATE</h3>
+        <h3 style="display: inline;">PIZZAGOUT</h3>
       </router-link>
     </div>
   </nav>
